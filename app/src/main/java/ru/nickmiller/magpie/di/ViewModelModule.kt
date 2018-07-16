75a0e066f38d46a923ffface7ab95659239376b4
Nick Miller <nickmiller.on@gmail.com>
2018-07-16 20:47:18 +0300

    update di

--- app/src/main/java/ru/nickmiller/magpie/di/ViewModelModule.kt
+++ app/src/main/java/ru/nickmiller/magpie/di/ViewModelModule.kt
@@ -7,20 +7,24 @@ import ru.magpie.magpie.data.net.NetHelper
 import ru.nickmiller.magpie.MagpieApp
 import ru.nickmiller.magpie.data.cache.AppDatabase
 import ru.nickmiller.magpie.data.cache.dao.ChannelsDao
+import ru.nickmiller.magpie.data.entity.article.ArticleMapper
+import ru.nickmiller.magpie.data.entity.feed.FeedMapper
 import ru.nickmiller.magpie.data.entity.feedChannel.FeedChannelMapper
+import ru.nickmiller.magpie.data.repository.ArticlesRepository
 import ru.nickmiller.magpie.data.repository.FeedChannelRepository
+import ru.nickmiller.magpie.data.repository.datasource.articles.CloudArticlesStore
+import ru.nickmiller.magpie.data.repository.datasource.articles.LocalArticlesStore
 import ru.nickmiller.magpie.data.repository.datasource.channels.CloudChannelStore
 import ru.nickmiller.magpie.data.repository.datasource.channels.LocalChannelStore
 import ru.nickmiller.magpie.ui.channels.ChannelsViewModel
+import ru.nickmiller.magpie.ui.feed.FeedViewModel
 import ru.nickmiller.magpie.ui.search.SearchViewModel
 
 val serachChannelModule = applicationContext {
     viewModel { SearchViewModel(androidApplication(), get()) }
     bean { FeedChannelRepository(get(), get(), get()) }
     bean { FeedChannelMapper() }
-    bean {
-        MagpieApp.database
-    }
+    bean { MagpieApp.database }
     bean { get<AppDatabase>().getChannelDao() }
     bean { LocalChannelStore(get(), get()) }
     bean { CloudChannelStore(get(), get()) }
@@ -29,6 +33,16 @@ val serachChannelModule = applicationContext {
 
 val channelsModule = applicationContext {
     viewModel { ChannelsViewModel(androidApplication(), get()) }
+}
+
+val feedModule = applicationContext {
+    viewModel { FeedViewModel(androidApplication(), get()) }
+    bean { ArticlesRepository(get(), get()) }
+    bean { LocalArticlesStore(get(), get()) }
+    bean { get<AppDatabase>().getArticlesDao() }
+    bean { CloudArticlesStore(get(), get(), get(), get()) }
+    bean { ArticleMapper() }
+    bean { FeedMapper(get()) }
 }
  
  
