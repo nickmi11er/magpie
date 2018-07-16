79c281c697fb86ac899700c68c7a0999aecce275
Nick Miller <nickmiller.on@gmail.com>
2018-07-16 20:47:52 +0300

    update db with articles dao

--- app/src/main/java/ru/nickmiller/magpie/data/cache/AppDatabase.kt
+++ app/src/main/java/ru/nickmiller/magpie/data/cache/AppDatabase.kt
@@ -3,13 +3,17 @@ package ru.nickmiller.magpie.data.cache
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
 import android.arch.persistence.room.TypeConverters
+import ru.nickmiller.magpie.data.cache.dao.ArticlesDao
 import ru.nickmiller.magpie.data.cache.dao.ChannelsDao
 import ru.nickmiller.magpie.data.cache.dao.Converters
+import ru.nickmiller.magpie.data.entity.article.ArticleEntity
+import ru.nickmiller.magpie.data.entity.feed.FeedEntity
 import ru.nickmiller.magpie.data.entity.feedChannel.FeedChannelEntity
 
 
-@Database(entities = [(FeedChannelEntity::class)], version = 1)
+@Database(entities = [(FeedChannelEntity::class), (ArticleEntity::class)], version = 2)
 @TypeConverters(value = [(Converters::class)])
 abstract class AppDatabase : RoomDatabase() {
     abstract fun getChannelDao(): ChannelsDao
+    abstract fun getArticlesDao(): ArticlesDao
 }
